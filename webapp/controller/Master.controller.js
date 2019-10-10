7508d09e9a778a2628116ab63253532ec4de9acf
S. Leijenhorst <sam.leijenhorst@ciber.nl>
2019-10-10 17:44:40 +0000

    End of Day

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -1,20 +1,75 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function (Controller, MessageToast) {
 	"use strict";
 
 	return Controller.extend("com.alliander.todo.demo.uren-schrijven-demo-pwa.controller.Master", {
-		onInit: function () {
-		},
+		onInit: function () {},
 		onPress: function (oEvent) {
-			this.getOwnerComponent().getRouter().navTo("Detail", 
-				{Item: oEvent.getSource().getBindingContext("todo").sPath.split("/").pop()});
+			this.getOwnerComponent().getRouter().navTo("Detail", {
+				Item: oEvent.getSource().getBindingContext("todo").sPath.split("/").pop()
+			});
 		},
 		onAdd: function (oEvent) {
-			this.getView().getModel("todo").addItem("newEntry", {Customer: "", Project: "", Time: "8:00"});
+			var oLongitude;
+			navigator.geolocation.getCurrentPosition(function (oCoords) {
+				oLongitude = oCoords.coords.longitude;
+				var sCustomer = oLongitude > 0 ? "Alliander" : "Vebego";
+				this.getView().getModel("todo").addItem("newEntry", {
+					Customer: "Alliander",
+					Project: "Development",
+					Time: "8:00"
+				});
+				MessageToast.show("Klant en project bepaald o.b.v. locatie.");
+
+			}.bind(this))
 		},
-		onRemove: function (oEvent){
-			
+		onLocationSelect: function (oEvent) {
+			MessageToast.show("Klant en project bepaald o.b.v. locatie.");
+		},
+		onRemove: function (oEvent) {
+			this.getView().getModel("todo").deleteItem(oEvent.oSource.oParent.oParent.getBindingContext("todo").getObject().id)
+		},
+		onCollapse: function (oEvent) {
+			var bVisible = oEvent.getSource().getParent().getParent().getContent()[1].getVisible();
+			oEvent.getSource().getParent().getParent().getContent()[1].setVisible(!bVisible);
+			oEvent.getSource().setIcon(bVisible ? "sap-icon://navigation-down-arrow" : "sap-icon://navigation-up-arrow");
+		},
+		handlePressOpenMenu: function (oEvent) {
+			var oButton = oEvent.getSource();
+
+			// create menu only once
+			if (!this._menu) {
+				this._menu = sap.ui.xmlfragment(
+					"com.alliander.todo.demo.uren-schrijven-demo-pwa.fragments.Menu",
+					this
+				);
+				this.getView().addDependent(this._menu);
+			}
+
+			var eDock = sap.ui.core.Popup.Dock;
+			this._menu.open(this._bKeyboard, oButton, eDock.BeginTop, eDock.BeginBottom, oButton);
+		},
+		onSubmit: function (oEvent) {
+			MessageToast.show("Uren zijn ingediend");
+		},
+
+		handleMenuItemPress: function(oEvent) {
+			switch (oEvent.getParameter("item").getId()){
+			case "MonthButton": 
+				this.getOwnerComponent().getRouter().navTo("Month");
+				break;
+			case "WeekButton": 
+				this.getOwnerComponent().getRouter().navTo("Detail");
+				break;
+			}
+		},
+		onNavToWeek: function (oEvent) {
+			this.getRouter().navTo("Detail");
+		},
+		onNavToMonth: function (oEvent) {
+			this.getRouter().navTo("Month");
 		}
 	});
 });
---
+++ webapp/controller/Month.controller.js
@@ -0,0 +1,65 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History",
+	"../model/firestore-todo"
+], function (Controller, History, firestoreTodo) {
+	"use strict";
+
+	return Controller.extend("com.alliander.todo.demo.uren-schrijven-demo-pwa.controller.Month", {
+		onInit: function () {
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("Month").attachMatched(this.onRouteMatched, this);
+		},
+		
+		_id: null,
+		onRouteMatched: function(oEvent) {
+			this._id = oEvent.getParameter("arguments").Item;
+			this.getView().bindElement({
+				path: "todo>/" + oEvent.getParameter("arguments").Item
+			});
+		},
+		
+		onCheckItem: function(oEvent) {
+		    var bSelected = oEvent.getParameter("selected");
+		    var oContext = oEvent.getSource().getBindingContext("todo");
+		    var selectedId = oContext.getModel().getProperty(oContext.getPath() + "/id");
+		    firestoreTodo.setSelected(selectedId, bSelected);
+		},
+		
+		onChangeTitle: function(oEvent) {
+			var oContext = oEvent.getSource().getBindingContext("todo");
+			var iId = oContext.getModel().getProperty(oContext.getPath() + "/id");
+			var sTitle = oContext.getModel().getProperty(oContext.getPath() + "/title");
+		    firestoreTodo.setTitle(iId, sTitle);
+		},
+		
+		onChangeDescription: function(oEvent) {
+			var oContext = oEvent.getSource().getBindingContext("todo");
+			var iId = oContext.getModel().getProperty(oContext.getPath() + "/id");
+			var sDescription = oContext.getModel().getProperty(oContext.getPath() + "/description");
+		    firestoreTodo.setDescription(iId, sDescription);
+		},
+		
+		onBack: function(){
+			var sPreviousHash = History.getInstance().getPreviousHash();
+			
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				this.getOwnerComponent().getRouter().navTo("RouteList", {}, true);
+			}
+		},
+		
+		onDelete: function() {
+			var oPage = this.getView().byId("TodoTaskPage"),
+			that = this;
+			oPage.setBusy(true);
+			firestoreTodo.deleteItem(this._id, {
+				success: function() {
+					oPage.setBusy(false);
+					that.onBack();
+				}
+			});
+		}
+	});
+});
